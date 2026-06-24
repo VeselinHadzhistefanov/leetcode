7792239b14cfe407d5c7cb9780f5550460086fbd
veselinhadzhistefanov <v.hadzhistefanov@gmail.com>
2026-06-24 14:40:31 +0300

    Provided better class hierarchy signatures

--- addTwoNumbers_digitsWithLinkedList.js
+++ addTwoNumbers_digitsWithLinkedList.js
@@ -11,26 +11,54 @@
 
 // Test data
 
-var LinkedListValue = function(value, next){
-  this.value = (value === undefined ? 0 : value);
-  this.next = (next === undefined ? null : next);
-}
-
-var getLinkedListNumber = function(l){
-  var number = 0;
-  var operator = 1;
-  var base = 10;
-
-  while(l.next !== null){
-    number += l.value * operator;
+class LinkedListValue {
+  constructor(value, next) {
+    this.value = (value === undefined ? 0 : value);
+    this.next = (next === undefined ? null : next);
   }
 }
 
-var addTwoNumbers = function(l1, l2) {
-  //var n1 =
+class LinkedList {
+  Node = class {
+    constructor(value, next) {
+      this.value = value ?? 0;
+      this.next = next ?? null;
+    }
+  }
 
-  return 5;
+  constructor(s) {
+    this.start = s
+  }
+
+  get = function () {
+    if (this.start === undefined) return 0;
+
+    let p = this.start;
+    let operator = 1;
+    let num = 0;
+
+    while (p != null) {
+      num += operator * p.value;
+      operator *= 10;
+      p = p.next;
+    }
+
+    return num;
+  }
+
+  put = function () {
+    return 0;
+  }
+
+  add = function () {
+    return 0;
+  }
+}
+
+// Leetcode test function
+var addTwoNumbers = function (l1, l2) {
+  let n1 = l1.get();
+  let n2 = l2.get();
+
+  return n1 + n2;
 };
-
-
-console.log(addTwoNumbers());
