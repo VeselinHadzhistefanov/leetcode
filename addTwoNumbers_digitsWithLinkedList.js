b1b6c7007121c51a63b6a327dc981623dd8819a4
veselinhadzhistefanov <v.hadzhistefanov@gmail.com>
2026-06-24 12:05:25 +0300

    Minor changes to method signatures

--- addTwoNumbers_digitsWithLinkedList.js
+++ addTwoNumbers_digitsWithLinkedList.js
@@ -4,12 +4,33 @@
  *     this.val = (val===undefined ? 0 : val)
  *     this.next = (next===undefined ? null : next)
  * }
- */
-/**
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
  */
+
+// Test data
+
+var LinkedListValue = function(value, next){
+  this.value = (value === undefined ? 0 : value);
+  this.next = (next === undefined ? null : next);
+}
+
+var getLinkedListNumber = function(l){
+  var number = 0;
+  var operator = 1;
+  var base = 10;
+
+  while(l.next !== null){
+    number += l.value * operator;
+  }
+}
+
 var addTwoNumbers = function(l1, l2) {
-    
+  //var n1 =
+
+  return 5;
 };
+
+
+console.log(addTwoNumbers());
